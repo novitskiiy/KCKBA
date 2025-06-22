a1eb735452357105a4fc3967b33fcc2c4e5e27e7
novitskiiy <veremienko.bogdan@gmail.com>
2025-06-22 20:29:06 +0300

    pages about and certificates

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,6 @@
 /*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2025 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
+ * KCKBA v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+ * Copyright 2024 Bogdan Veremienko
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
